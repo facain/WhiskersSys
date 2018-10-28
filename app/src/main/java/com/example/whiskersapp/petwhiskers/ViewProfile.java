844141ecb8944c4e83fc8641d9b48436314a6805
erynvinnice <erynvinnice@gmail.com>
2018-10-28 19:01:05 +0800

    New

---
+++ app/src/main/java/com/example/whiskersapp/petwhiskers/ViewProfile.java
@@ -0,0 +1,16 @@
+package com.example.whiskersapp.petwhiskers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ViewProfile extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
